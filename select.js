e45aa938b011061440d548897cf6e5d07f4520fd
oeanb12 <oeanb12@gmail.com>
2021-10-11 23:29:24 +0900

    Finish

--- select.js
+++ select.js
@@ -1,9 +1,11 @@
 $('#gs25').hide();
+$('#gift_guide').hide();
 
 $('#one').on('click', function() {
     $('#one').hide();
     $('#two').hide();
     $('#gs25').show();
+    $('#gift_guide').show();
 })
 
 $('#gs25').on('click', function() {
