6d57b5b3da470074521755af345d57ff752df92d
oeanb12 <oeanb12@gmail.com>
2021-10-14 13:55:39 +0900

    Finishgit add .!

--- select.js
+++ select.js
@@ -1,16 +1,19 @@
-$('#gs25').hide();
-$('#gift_guide').hide();
-
+//첫번째 선물 상자를 누르면 패스워드 입력 칸이 나옴.
 $('#one').on('click', function() {
-    $('#one').hide();
-    $('#two').hide();
-    $('#gs25').show();
-    $('#gift_guide').show();
+    $('form').show();
+    $('#sub').on('click', getGift);
 })
 
-$('#gs25').on('click', function() {
-    $('#one').show();
-    $('#two').show();
-    $('#gs25').hide();
-    $('#gift_guide').hide();
+$('#two').on('click', function() {
+    $('form').show();
+    $('#sub').on('click', getGift);
 })
+
+//패스워드를 입력하면 기프티콘 이미지 창이 뜸.
+function getGift() {
+    let pwd = $('#pwd').val();
+    if(pwd === '1995') 
+        window.open('images/gs25.jpg');
+    else if(pwd === '1024')
+        window.open('images/cafe.jpg');
+}
