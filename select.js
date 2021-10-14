f04905dc8c86a68e3220d68ab7d10321ba649af8
oeanb12 <oeanb12@gmail.com>
2021-10-14 14:37:07 +0900

    Finishgit add .!

--- select.js
+++ select.js
@@ -1,12 +1,14 @@
 //첫번째 선물 상자를 누르면 패스워드 입력 칸이 나옴.
 $('#one').on('click', function() {
+    $('.sub').removeClass('cafe');
     $('form').show();
-    $('#sub').on('click', getGift);
+    $('.sub').on('click', getGift);
 })
 
 $('#two').on('click', function() {
+    $('.sub').addClass('cafe');
     $('form').show();
-    $('#sub').on('click', getGift);
+    $('.sub').on('click', getGift);
 })
 
 //패스워드를 입력하면 기프티콘 이미지 창이 뜸.
