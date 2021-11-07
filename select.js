e904adad278f5c5953d408ac2c1388d0f3dbb6e0
oeanb12 <oeanb12@gmail.com>
2021-11-08 02:04:04 +0900

    Amend lastThing

--- select.js
+++ select.js
@@ -2,20 +2,24 @@
 $('#one').on('click', function() {
     $('.sub').removeClass('cafe');
     $('form').show();
-    $('.sub').on('click', getGift);
+    $('.sub').on('click', getGift1);
 })
 
 $('#two').on('click', function() {
     $('.sub').addClass('cafe');
     $('form').show();
-    $('.sub').on('click', getGift);
+    $('.sub').on('click', getGift2);
 })
 
 //패스워드를 입력하면 기프티콘 이미지 창이 뜸.
-function getGift() {
+function getGift1() {
     let pwd = $('#pwd').val();
     if(pwd === '1995') 
         window.open('images/gs25.jpg');
-    else if(pwd === '1024')
+}
+
+function getGift2() {
+    let pwd = $('#pwd').val();
+    if(pwd === '1024')
         window.open('images/cafe.jpg');
 }
